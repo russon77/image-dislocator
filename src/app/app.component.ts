44f9fd16906700b0bf02471dbceb8d6620961f07
Tristan Kernan <russon77@gmail.com>
2018-01-02 19:42:24 -0500

    feat: setup, initial design, initial work on image manipulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+declare const thread: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public baseImage: string = null;
+  public changedImage: string = null;
+  public isLoading = false;
+
+  constructor() {}
+
+  public onSelectImage(file: File): void {
+    const reader = new FileReader();
+
+    reader.onload = (e: any) => {
+      this.baseImage = e.target.result;
+      this.changedImage = null;
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  public onDislocateChange({checked}: { checked: boolean }): void {
+    if (!checked) {
+      this.changedImage = null;
+
+      return;
+    }
+
+    this.isLoading = true;
+
+    Observable.create(
+      observer => {
+        const canvas: HTMLCanvasElement = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        const baseImage = new Image();
+
+        baseImage.onload = () => {
+          canvas.width = baseImage.width;
+          canvas.height = baseImage.height;
+
+          ctx.drawImage(baseImage, 0, 0);
+
+          // web worker - later!
+
+          // retrieve pixel data
+          const baseImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+          baseImageData.data.sort();
+
+          ctx.putImageData(baseImageData, 0, 0);
+
+          // sort
+          // baseImageData.data.sort();
+
+          // console.log(baseImageData.data);
+
+          // draw pixel array to result canvas
+          // changedCtx.putImageData(baseImageData, 0, 0);
+
+          // finally, return the canvas as an image
+          observer.next(canvas.toDataURL('image/png'));
+          observer.complete();
+        };
+
+        baseImage.src = this.baseImage;
+      }
+    )
+      .subscribe(
+        changedImage => {
+          this.changedImage = changedImage;
+          this.isLoading = false;
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule, MatProgressBarModule, MatSlideToggleModule, MatToolbarModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    /* Angular Material */
+    MatToolbarModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
